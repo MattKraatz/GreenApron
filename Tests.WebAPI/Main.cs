57a6eadccb984c6fd12c70c528bffe3e2922762f
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-06 09:56:20 -0800

    added WebAPI project, test projects, cleaned up directory

---
+++ Tests.WebAPI/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.WebAPI
+{
+    public class Main
+    {
+        [Fact]
+        public void FrameworkInstantiates()
+        {
+            Assert.Null(null);
+            Assert.True(true);
+            Assert.NotNull(1);
+        }
+    }
+}
