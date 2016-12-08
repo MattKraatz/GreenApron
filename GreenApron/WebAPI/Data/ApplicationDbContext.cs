74b168a2d46a749657f3735651e64279123f9236
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-08 13:39:05 -0600

    trying just with webapi now

---
+++ GreenApron/WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUser>()
+                .Property(b => b.DateCreated)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        }
+    }
+}
