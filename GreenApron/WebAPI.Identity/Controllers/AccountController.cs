a17469db23f9435db1922c3092a1b3b28a05656e
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-08 13:24:25 -0600

    added identity framework

---
+++ GreenApron/WebAPI.Identity/Controllers/AccountController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using WebAPI.Models;
+using WebAPI.Models.AccountViewModels;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
+        private readonly ISmsSender _smsSender;
+        private readonly ILogger _logger;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ISmsSender smsSender,
+            ILoggerFactory loggerFactory)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _smsSender = smsSender;
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation(1, "User logged in.");
+                    var user = await GetCurrentUserAsync();
+                    return Json(new { success = "true", message = "You have logged in.", user = user});
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return Json(new { success = "false", message = "This user requires two-factor authentication."});
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning(2, "User account locked out.");
+                    return Json(new { success = "false", message = "User account locked out." });
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Json(model);
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Json(new { success = "false", message = "Something went wrong."});
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    user = await GetCurrentUserAsync();
+                    _logger.LogInformation(3, "User created a new account with password.");
+                    return Json(new { success="true", message = "You have registered successfully.", user = user});
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed
+            return Json(new { success = "false", message = "Something went wrong." });
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation(4, "User logged out.");
+            return Json(new { success = "true" });
+        }
+
+        //
+        // POST: /Account/ForgotPassword
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user == null || !(await _userManager.IsEmailConfirmedAsync(user)))
+                {
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return Json(new { success = "true" });
+                }
+
+                // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=532713
+                // Send an email with this link
+                //var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                //var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                //await _emailSender.SendEmailAsync(model.Email, "Reset Password",
+                //   $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+                //return View("ForgotPasswordConfirmation");
+            }
+
+            // If we got this far, something failed
+            return Json(new { success = "false" });
+        }
+
+        //
+        // POST: /Account/ResetPassword
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { success = "false" });
+            }
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user == null)
+            {
+                // Don't reveal that the user does not exist
+                return Json(new { success = "true"});
+            }
+            var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
+            if (result.Succeeded)
+            {
+                return Json(new { success = "true" });
+            }
+            AddErrors(result);
+            return Json(new { success = "false" });
+        }
+
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        #endregion
+    }
+}
---
+++ Tests.WebAPI/AuthTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPI.Data;
+using WebAPI.Controllers;
+using WebAPI.Models;
+
+namespace Tests.WebAPI
+{
+    public class AuthTests
+    {
+        static ApplicationDbContext _context;
+        public AuthTests()
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddEntityFrameworkSqlServer()
+            .BuildServiceProvider();
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connection = Environment.GetEnvironmentVariable("GreenApronDB");
+            builder.UseSqlServer(connection)
+                    .UseInternalServiceProvider(serviceProvider);
+            _context = new ApplicationDbContext(builder.Options);
+            _context.Database.Migrate();
+        }
+
+        Guid userId = new Guid();
+
+        [Fact]
+        public void CanRegisterNewUser()
+        {
+
+        }
+
+        [Fact]
+        public void CanLoginUser()
+        {
+
+        }
+
+        [Fact]
+        public void CanDeleteUser()
+        {
+
+        }
+    }
+}
