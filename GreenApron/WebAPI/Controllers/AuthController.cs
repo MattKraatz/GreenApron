8b8221c6cc3501832126602f1c5d2b4a9c79da45
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-11 09:45:17 -0600

    login functionality complete

--- GreenApron/WebAPI/Controllers/AuthController.cs
+++ GreenApron/WebAPI/Controllers/AuthController.cs
@@ -30,7 +30,7 @@ namespace WebAPI.Controllers
             if (!ModelState.IsValid)
             {
                 // If invalid, return error message
-                return Json(new { success = "false", message = "Something went wrong, please resubmit with all required fields." });
+                return Json(new JsonResponse{ success = false, message = "Something went wrong, please resubmit with all required fields." });
             }
             // Check for existing username in database
             User userCheck = await _context.User.SingleOrDefaultAsync(u => u.Username == user.Username);
@@ -38,14 +38,14 @@ namespace WebAPI.Controllers
             {
                 if (userCheck.Password == user.Password)
                 {
-                    return Json(new { success = "true", message = "You have successfully logged in.", user = userCheck });
+                    return Json(new JsonResponse{ success = true, message = "You have successfully logged in.", user = userCheck });
                 } else
                 {
-                    return Json(new { success = "false", message = "Wrong password, please try again" });
+                    return Json(new JsonResponse{ success = false, message = "Wrong password, please try again" });
                 }
             } else
             {
-                return Json(new { success = "false", message = "I couldn't find a user with that username, please try again." });
+                return Json(new JsonResponse{ success = false, message = "I couldn't find a user with that username, please try again." });
             }
         }
 
@@ -57,7 +57,7 @@ namespace WebAPI.Controllers
             if (!ModelState.IsValid)
             {
                 // If invalid, return error message
-                return Json(new { success = "false", message = "Something went wrong, please resubmit with all required fields." });
+                return Json(new JsonResponse{ success = false, message = "Something went wrong, please resubmit with all required fields." });
             }
             // Check for existing username in database
             User usernameCheck = await _context.User.SingleOrDefaultAsync(u => u.Username == user.Username);
@@ -68,23 +68,39 @@ namespace WebAPI.Controllers
                 try
                 {
                     await _context.SaveChangesAsync();
-                    return Json(new { success = "true", message = "You have successfully registered as a new user.", user = user });
+                    return Json(new JsonResponse{ success = true, message = "You have successfully registered as a new user.", user = user });
                 }
                 catch
                 {
-                    return Json(new { success = "false", message = "Something went wrong while saving to the database, please try again." });
+                    return Json(new JsonResponse{ success = false, message = "Something went wrong while saving to the database, please try again." });
                 }
             } else
             {
-                return Json(new { success = "false", message = "A user already exists with this username, please try again." });
+                return Json(new JsonResponse{ success = false, message = "A user already exists with this username, please try again." });
             }
         }
 
-        // DELETE api/values
+        // DELETE api/auth/delete/username
         [HttpDelete("{username}")]
-        public IActionResult Delete(string username)
+        public async Task<JsonResult> Delete(string username)
         {
-            return Json(new { value = "value" });
+            User user = await _context.User.SingleOrDefaultAsync(u => u.Username == username);
+            if (user != null)
+            {
+                _context.User.Remove(user);
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return Json(new JsonResponse{ success = true, message = "User successfully deleted" });
+                }
+                catch
+                {
+                    return Json(new JsonResponse{ success = false, message = "Database updates failed, please try again." });
+                }
+            } else
+            {
+                return Json(new JsonResponse{ success = false, message = "User not found." });
+            }
         }
     }
 }
