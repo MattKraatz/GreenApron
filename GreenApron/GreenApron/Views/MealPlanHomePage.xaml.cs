0d98eb5bceb86a3b83bf6382956de993e2f629ad
Matt Kraatz <matt.kraatz@gmail.com>
2016-12-30 00:26:10 -0600

    refactored Meal Plan page into scroll view

--- GreenApron/GreenApron/Views/MealPlanHomePage.xaml.cs
+++ GreenApron/GreenApron/Views/MealPlanHomePage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Syncfusion.SfCalendar.XForms;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using System.Windows.Input;
 
 namespace GreenApron
 {
@@ -19,50 +21,40 @@ namespace GreenApron
         public MealPlanHomePage()
         {
             InitializeComponent();
-            calendar.OnCalendarTapped += OnCalendarTapped;
-            layout.Children.Add(calendar);
+            //calendar.OnCalendarTapped += OnCalendarTapped;
+            //layout.Children.Add(calendar);
             GetMealPlans();
         }
 
         public async void GetMealPlans()
         {
             response = await App.APImanager.GetActivePlans();
-            // Add a calendar appointment for each plan retrieved from the WebAPI
-            foreach (Plan plan in response.plans)
-            {
-                CalendarInlineEvent events = new CalendarInlineEvent();
-                switch(plan.Meal)
-                {
-                    case "Breakfast":
-                        events.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 7, 0, 0);
-                        events.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 8, 0, 0);
-                        events.Color = Color.Yellow;
-                        break;
-                    case "Lunch":
-                        events.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 11, 0, 0);
-                        events.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 12, 0, 0);
-                        events.Color = Color.Blue;
-                        break;
-                    case "Snack":
-                        events.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 14, 0, 0);
-                        events.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 15, 0, 0);
-                        events.Color = Color.Olive;
-                        break;
-                    case "Dinner":
-                        events.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 18, 0, 0);
-                        events.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 19, 0, 0);
-                        events.Color = Color.Red;
-                        break;
-                    case "Dessert":
-                        events.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 20, 0, 0);
-                        events.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 21, 0, 0);
-                        events.Color = Color.Teal;
-                        break;
-                }
-                events.Subject = plan.Meal + " - " + plan.RecipeName;
-                collection.Add(events);
-            }
-            calendar.DataSource = collection;
+            // Print the page with meal plans
+            PrintDays(response.plans);
+            //// Add Appointments to the calendar
+            //foreach (Plan plan in response.plans)
+            //{
+            //    AddAppointment(plan);
+            //}
+            //calendar.DataSource = collection;
+        }
+
+        public async void OnAddaPlanClicked(object sender, EventArgs e)
+        {
+            var page = new RecipeSearchPage();
+            await Navigation.PushAsync(page);
+        }
+
+        public async void OnAddClicked(DateTime date)
+        {
+            var page = new RecipeSearchPage();
+            await Navigation.PushAsync(page);
+        }
+
+        public async void OnEditClicked(Plan plan)
+        {
+            var page = new PlanPage(plan);
+            await Navigation.PushAsync(page);
         }
 
         public async void OnCalendarTapped(object sender, Syncfusion.SfCalendar.XForms.CalendarTappedEventArgs args)
@@ -79,6 +71,120 @@ namespace GreenApron
                 var newPage = new PlanPage(plan);
                 await Navigation.PushAsync(newPage);
             }
+        }
+
+        public void PrintDays(List<Plan> plans)
+        {
+            for (var i = 0; i < 30; i ++)
+            {
+                var today = DateTime.Today.AddDays(i);
+                // Create a label for the date
+                var dateLabel = new Label
+                {
+                    Text = today.ToString("dddd, dd MMMM"),
+                    TextColor = Color.FromHex("#008A09"),
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
+                };
+                // Add Date label to the stack
+                scrollStack.Children.Add(dateLabel);
+
+                var todayPlans = plans.Where(p => p.Date.Date == today.Date).ToList();
+                if (todayPlans.Count > 0)
+                {
+                    PrintPlans(todayPlans, "Breakfast");
+                    PrintPlans(todayPlans, "Lunch");
+                    PrintPlans(todayPlans, "Snack");
+                    PrintPlans(todayPlans, "Dinner");
+                    PrintPlans(todayPlans, "Dessert");
+                }
+                var addButton = new Button { Text = "Add a Plan", TextColor = Color.White, BackgroundColor = Color.FromHex("#50F75B") };
+                addButton.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => OnAddClicked(today)) });
+                scrollStack.Children.Add(addButton);
+            }
+        }
+
+        public void PrintPlans(List<Plan> plans, string meal)
+        {
+            var meals = plans.Where(p => p.Meal == meal).ToList();
+            if (meals.Count > 0)
+            {
+                var mealLabel = new Label
+                {
+                    Text = meal,
+                    TextColor = Color.FromHex("#AF0400"),
+                    HorizontalTextAlignment = TextAlignment.Start
+                };
+                scrollStack.Children.Add(mealLabel);
+                foreach (Plan plan in meals)
+                {
+                    var planCard = new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Padding = new Thickness(20, 5),
+                        Children =
+                        {
+                            new StackLayout
+                            {
+                                HorizontalOptions = LayoutOptions.Start,
+                                VerticalOptions = LayoutOptions.Start,
+                                Children =
+                                {
+                                    new Image { HeightRequest = 50, WidthRequest = 80, Source = "https://spoonacular.com/cdn/recipes_100x100/Creamy-Tomato-Tortellini-Soup-548458.jpg" }
+                                }
+                            },
+                            new StackLayout
+                            {
+                                HorizontalOptions = LayoutOptions.StartAndExpand,
+                                VerticalOptions = LayoutOptions.StartAndExpand,
+                                Children =
+                                {
+                                    new Label { Text = plan.RecipeName, VerticalOptions = LayoutOptions.Start },
+                                    new Label { Text = "tap to view"}
+                                }
+                            }
+                        }
+                    };
+                    planCard.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => OnEditClicked(plan)) });
+                    scrollStack.Children.Add(planCard);
+                }
+            }
+        }
+
+        public void AddAppointment(Plan plan)
+        {
+            // Add a calendar appointment for each plan retrieved from the WebAPI
+            CalendarInlineEvent appt = new CalendarInlineEvent();
+            switch (plan.Meal)
+            {
+                case "Breakfast":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 7, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 8, 0, 0);
+                    appt.Color = Color.Yellow;
+                    break;
+                case "Lunch":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 11, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 12, 0, 0);
+                    appt.Color = Color.Blue;
+                    break;
+                case "Snack":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 14, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 15, 0, 0);
+                    appt.Color = Color.Olive;
+                    break;
+                case "Dinner":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 18, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 19, 0, 0);
+                    appt.Color = Color.Red;
+                    break;
+                case "Dessert":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 20, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 21, 0, 0);
+                    appt.Color = Color.Teal;
+                    break;
+            }
+            appt.Subject = plan.Meal + " - " + plan.RecipeName;
+            collection.Add(appt);
         }
     }
 }
