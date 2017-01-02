d5369a9f47e385dd7fdc9abcb95069775af33267
Matt Kraatz <matt.kraatz@gmail.com>
2017-01-01 23:20:07 -0600

    added plans as appointments on calendar view

--- GreenApron/GreenApron/Views/AddToPlanFromRecipePage.xaml.cs
+++ GreenApron/GreenApron/Views/AddToPlanFromRecipePage.xaml.cs
@@ -13,19 +13,47 @@ namespace GreenApron
     {
         public DateTime selectedDate { get; set; }
         public Recipe selectedRecipe { get; set; }
+        private SfCalendar _calendar { get; set; } = new SfCalendar {
+            MinDate = DateTime.Today, MaxDate = DateTime.Today.AddDays(14),
+            ShowInlineEvents = true
+        };
+        private MonthViewSettings _calendarSettings { get; set; } = new MonthViewSettings {
+            TodayTextColor = Color.FromHex("#008A09"), InlineTextColor = Color.Black,
+            InlineBackgroundColor = Color.FromHex("#FFC9C8"), SelectedDayTextColor = Color.FromHex("#008A09"),
+            DateSelectionColor = Color.FromHex("#FFC9C8")
+        };
+        private CalendarEventCollection _collection { get; set; } = new CalendarEventCollection();
 
         public AddToPlanFromRecipePage(Recipe recipe)
         {
             InitializeComponent();
             selectedRecipe = recipe;
             // Instantiate Calendar and add to View
-            SfCalendar calendar = new SfCalendar();
-            calendar.OnCalendarTapped += CacheSelectedDate;
-            layout.Children.Add(calendar);
+            _calendar.OnCalendarTapped += CacheSelectedDate;
+            _calendar.MonthViewSettings = _calendarSettings;
+            _calendar.VerticalOptions = LayoutOptions.FillAndExpand;
+            layout.Children.Add(_calendar);
             // Instantiate Add to Plan button and add to View
-            var addButton = new Button { Text = "Add to Plan", BackgroundColor = Color.FromHex("#00DE0E"), TextColor = Color.White };
+            var addButton = new Button { Text = "Add to Plan", BackgroundColor = Color.FromHex("#00DE0E"),
+                HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.White };
             addButton.Clicked += OnAddToPlanClicked;
             layout.Children.Add(addButton);
+            GetMealPlans();
+        }
+
+        public async void GetMealPlans()
+        {
+            var response = await App.APImanager.GetActivePlans();
+            if (response.success)
+            {
+                // Add Appointments to the calendar
+                foreach (Plan plan in response.plans)
+                {
+                    AddAppointment(plan);
+                }
+                _calendar.DataSource = _collection;
+            }
         }
 
         public async void OnAddToPlanClicked(object sender, EventArgs e)
@@ -53,6 +81,42 @@ namespace GreenApron
         public void CacheSelectedDate(object sender, Syncfusion.SfCalendar.XForms.CalendarTappedEventArgs args)
         {
             selectedDate = args.datetime;
+        }
+
+        public void AddAppointment(Plan plan)
+        {
+            // Add a calendar appointment for each plan retrieved from the WebAPI
+            CalendarInlineEvent appt = new CalendarInlineEvent();
+            switch (plan.Meal)
+            {
+                case "Breakfast":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 7, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 8, 0, 0);
+                    appt.Color = Color.FromHex("#FFC9C8");
+                    break;
+                case "Lunch":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 11, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 12, 0, 0);
+                    appt.Color = Color.FromHex("#FF5652");
+                    break;
+                case "Snack":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 14, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 15, 0, 0);
+                    appt.Color = Color.FromHex("#50F75B"); ;
+                    break;
+                case "Dinner":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 18, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 19, 0, 0);
+                    appt.Color = Color.FromHex("#008A09"); ;
+                    break;
+                case "Dessert":
+                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 20, 0, 0);
+                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 21, 0, 0);
+                    appt.Color = Color.FromHex("#AF0400"); ;
+                    break;
+            }
+            appt.Subject = plan.Meal + " - " + plan.RecipeName;
+            _collection.Add(appt);
         }
     }
 }
--- GreenApron/GreenApron/Views/MealPlanHomePage.xaml.cs
+++ GreenApron/GreenApron/Views/MealPlanHomePage.xaml.cs
@@ -14,15 +14,11 @@ namespace GreenApron
     public partial class MealPlanHomePage : ContentPage
     {
         CalendarEventCollection collection = new CalendarEventCollection();
-        SfCalendar calendar = new SfCalendar();
-        DateTime selectedDate;
         PlanResponse response;
 
         public MealPlanHomePage()
         {
             InitializeComponent();
-            //calendar.OnCalendarTapped += OnCalendarTapped;
-            //layout.Children.Add(calendar);
             GetMealPlans();
         }
 
@@ -40,12 +36,6 @@ namespace GreenApron
                 addButton.Command = new Command(OnAddClicked);
                 scrollStack.Children.Add(addButton);
             }
-            //// Add Appointments to the calendar
-            //foreach (Plan plan in response.plans)
-            //{
-            //    AddAppointment(plan);
-            //}
-            //calendar.DataSource = collection;
         }
 
         public async void OnAddClicked()
@@ -81,23 +71,23 @@ namespace GreenApron
             var page = new PlanPage(plan);
             await Navigation.PushAsync(page);
         }
-
-        public async void OnCalendarTapped(object sender, Syncfusion.SfCalendar.XForms.CalendarTappedEventArgs args)
-        {
-            selectedDate = args.datetime;
-            var planNames = response.plans.Where(p => p.Date.Date == selectedDate.Date).Select(p => p.Meal + " - " + p.RecipeName).ToArray();
-            var selectedMeal = await DisplayActionSheet("Which Meal?","Cancel",null,planNames);
-            if (planNames.Contains(selectedMeal))
-            {
-                var plan = response.plans.FirstOrDefault(p => (p.Meal + " - " + p.RecipeName) == selectedMeal);
-                // Grab recipe from Spoonacular
-                plan.Recipe = await App.SpoonManager.GetRecipeByIdAsync(plan.RecipeId);
-                // Instantiate new recipe page with plan as context
-                var newPage = new PlanPage(plan);
-                await Navigation.PushAsync(newPage);
-            }
-        }
-
+        
+        //public async void OnCalendarTapped(object sender, Syncfusion.SfCalendar.XForms.CalendarTappedEventArgs args)
+        //{
+        //    selectedDate = args.datetime;
+        //    var planNames = response.plans.Where(p => p.Date.Date == selectedDate.Date).Select(p => p.Meal + " - " + p.RecipeName).ToArray();
+        //    var selectedMeal = await DisplayActionSheet("Which Meal?","Cancel",null,planNames);
+        //    if (planNames.Contains(selectedMeal))
+        //    {
+        //        var plan = response.plans.FirstOrDefault(p => (p.Meal + " - " + p.RecipeName) == selectedMeal);
+        //        // Grab recipe from Spoonacular
+        //        plan.Recipe = await App.SpoonManager.GetRecipeByIdAsync(plan.RecipeId);
+        //        // Instantiate new recipe page with plan as context
+        //        var newPage = new PlanPage(plan);
+        //        await Navigation.PushAsync(newPage);
+        //    }
+        //}
+        
         public void PrintDays(List<Plan> plans)
         {
             for (var i = 0; i < 15; i ++)
@@ -180,42 +170,6 @@ namespace GreenApron
                     scrollStack.Children.Add(planCard);
                 }
             }
-        }
-
-        public void AddAppointment(Plan plan)
-        {
-            // Add a calendar appointment for each plan retrieved from the WebAPI
-            CalendarInlineEvent appt = new CalendarInlineEvent();
-            switch (plan.Meal)
-            {
-                case "Breakfast":
-                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 7, 0, 0);
-                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 8, 0, 0);
-                    appt.Color = Color.Yellow;
-                    break;
-                case "Lunch":
-                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 11, 0, 0);
-                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 12, 0, 0);
-                    appt.Color = Color.Blue;
-                    break;
-                case "Snack":
-                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 14, 0, 0);
-                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 15, 0, 0);
-                    appt.Color = Color.Olive;
-                    break;
-                case "Dinner":
-                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 18, 0, 0);
-                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 19, 0, 0);
-                    appt.Color = Color.Red;
-                    break;
-                case "Dessert":
-                    appt.StartTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 20, 0, 0);
-                    appt.EndTime = new DateTime(plan.Date.Year, plan.Date.Month, plan.Date.Day, 21, 0, 0);
-                    appt.Color = Color.Teal;
-                    break;
-            }
-            appt.Subject = plan.Meal + " - " + plan.RecipeName;
-            collection.Add(appt);
         }
     }
 }
